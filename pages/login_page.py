badeac17edbba36b0b1ea30523d706b772af0372
olesia <tiraspol4anka>
2020-02-11 16:31:10 +0300

    files changed

--- pages/login_page.py
+++ pages/login_page.py
@@ -10,14 +10,13 @@ class LoginPage(BasePage):
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
         assert "login" in self.browser.current_url, "login not founded"
-       # assert True
+
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login_form is not presented"
-       # assert True
+
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
         assert self.is_element_present(*LoginPageLocators.REGISTR_FORM), "registr_form is not presented"
-        #assert True
--- test_main_page.py
+++ test_main_page.py
@@ -7,6 +7,9 @@ def test_quest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
 
 
 def test_guest_should_see_login_link(browser):
