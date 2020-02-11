5f50bcd374390a9345f2e8be136826461fa9c80b
olesia <tiraspol4anka>
2020-02-11 14:40:03 +0300

    added test of login

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,14 @@
-link = "http://selenium1py.pythonanywhere.com/"
+from .pages.main_page import MainPage
+
+
+
 
 
 def test_quest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
 
 
 def go_to_login_page(browser):
